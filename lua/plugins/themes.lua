76398316c83281339974fd737e72f5f42535d9bf
oliRenan <oliveirarenan479@gmail.com>
2025-01-15 17:46:15 -0300

    arrumando coisas

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "rose-pine/neovim", name = "rose-pine",
+    --config = function()
+     -- vim.cmd.colorscheme "rose-pine"
+    --end 
+  },
+  {
+   "nyoom-engineering/oxocarbon.nvim" , name="oxocarbon",
+    config = function()
+      vim.cmd.colorscheme "oxocarbon"
+    end
+  }
+}
+
