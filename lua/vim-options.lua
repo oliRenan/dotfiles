28bbb74170c0c53a48fd324c55e4bc1739ae682c
oliRenan <oliveirarenan479@gmail.com>
2025-03-05 17:50:36 -0300

    refatorando minha config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.g.mapleader = " "
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -11,7 +10,7 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.colorcolumn = "160"
+vim.keymap.set("n", "<C-b>", function() vim.cmd("NvimTreeToggle") end)
 
 vim.keymap.set("v","J",":m '>+1<CR>gv=gv")
 vim.keymap.set("v","K",":m '<-2<CR>gv=gv")
